284938337a53390d07729483d2d3f59383d70633
jordan <jord.navratil@gmail.com>
2015-08-07 15:21:12 -0700

    correct variable names

---
+++ app/app.php
@@ -0,0 +1,34 @@
+<?php
+        require_once __DIR__."/../vendor/autoload.php";
+        require_once __DIR__."/../src/People.php";
+
+        session_start();
+        if (empty($_SESSION['address_book']))
+        {
+            $_SESSION['address_book'] = array();
+        };
+
+        $app = new Silex\Application();
+        $app['debug'] = true;
+        $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
+        ));
+
+        $app->get("/", function() use ($app) {
+            return $app['twig']->render('address_book.html.twig', array('people' => People::getAll()));
+        });
+
+        $app->post("/people", function() use ($app) {
+            $person = new Person($_POST['name'], $_POST['phone_number'], $_POST['address']);
+            $person->save();
+            return $app['twig']->render('add_contact.html.twig', array('newcontact' => $person));
+
+        });
+
+        $app->post("/delete_contacts", function() use ($app){
+            Place::deleteAll();
+            return $app('twig')->render('delete_contacts.html.twig');
+        });
+
+    return $app;
+
+?>
--- src/people.php
+++ src/people.php
@@ -2,10 +2,51 @@
 class People
 {
     private $name;
-    private $number;
+    private $phone_number;
     private $address;
+    function __construct($person_name, $person_phone_number, $person_address)
+
+    {
+        $this->name = $add_name;
+        $this->phone_number = $add_phone_number;
+        $this->address = $add_address;
+    }
+    function setName ($new_name)
+    {
+        $this->name = (string) $new_name;
+    }
+    function getName ()
+    {
+        return $this->name;
+    }
+    function setPhone_Number ($new_phone_number)
+    {
+        $this->setPhone_Number = (int) $new_phone_number;
+    }
+    function getPhone_Number ()
+    {
+        return $this->phone_number;
+    }
+    function setAddress($new_address)
+    {
+        $this->address = (string) $new_address;
+    }
+    function getAddress()
+    {
+        return $this->address;
+    }
+    function save()
+    {
+        array_push($_SESSION['address_book'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['address_book'];
+    }
+    static function deleteAll()
+    {
+        $_SESSION['address_book']= array();
+    }
 }
-
-
-
 ?>
